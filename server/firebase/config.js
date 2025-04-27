59c343179d754e63f426bf6ebde4a5fbfb79f17c
Вадим <69336443+IOMOIO-hub@users.noreply.github.com>
2025-04-27 17:45:52 +0500

    oops

--- server/firebase/config.js
+++ server/firebase/config.js
@@ -1,10 +1,1 @@
-export const secret = "ridiculous_key"
-export const firebaseConfig = {
-    apiKey: "AIzaSyCtc43xYOdsGkmoG3O19127ycq_J4UfKP0",
-    authDomain: "collegeleads-b120e.firebaseapp.com",
-    projectId: "collegeleads-b120e",
-    storageBucket: "collegeleads-b120e.appspot.com",
-    messagingSenderId: "986890435549",
-    appId: "1:986890435549:web:378470c278b4eec138922c",
-    measurementId: "G-QC8LFF9FCX"
-};
+
