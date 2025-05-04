0f26a873007f6eb2bb60d826ea1f6fe2fd6fbfd9
Vadim Karataev <karataev.vadim.a@gmail.com>
2025-05-04 20:11:02 +0500

    Фавиконка и яндекс метрика

--- client/src/components/landing/Modal.vue
+++ client/src/components/landing/Modal.vue
@@ -88,6 +88,7 @@ export default {
 
 <style lang="scss" scoped>
 .modal-overlay {
+  max-width: 100%;
 	position: fixed;
 	top: 0; left: 0; right: 0; bottom: 0;
 	background-color: rgba(0, 0, 0, 0.5);
